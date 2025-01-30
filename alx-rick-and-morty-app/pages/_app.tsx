c5a1bd4da854f01bc0b5c2f642cec51b7321d667
front-end-geek <lovepreetbrands@gmail.com>
2025-01-30 09:29:23 +0300

    error handling

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -2,11 +2,17 @@ import client from '@/graphql/apolloClient';
 import '@/styles/globals.css';
 import { ApolloProvider } from '@apollo/client';
 import type { AppProps } from 'next/app';
+import ErrorBoundary from '@/components/ErrorBoundary';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <ErrorBoundary>
     <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
+    </ErrorBoundary>
   );
 }
+
+
